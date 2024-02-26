8e3e978031e71d0bc91b9424effeb3a120202bdb
vEveriAxas <v.everi@axas.ru>
2024-02-26 08:24:07 +0300

    last-commit

--- src/store/general.js
+++ src/store/general.js
@@ -1,7 +1,6 @@
 import { defineStore } from "pinia";
 
 
-
 const useGeneralStore = defineStore("generalStore", () => {
     // Функция применяется для форматирования ключей объектов из формата snake_case в camelCase
     function convertKeysToCamelCase(obj) {
